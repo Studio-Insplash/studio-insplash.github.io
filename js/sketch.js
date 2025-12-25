800de6ab21c23582a6a63a0df6f68aac6e56d352
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2025-12-25 23:56:13 +0900

    Adjust padlock log shape
    Refine its outline and proportions to improve visual balance.

--- js/sketch.js
+++ js/sketch.js
@@ -8,13 +8,13 @@ function draw() {
     translate(width / 2, height / 2); // 原点が画面中央になる。
     
     stroke(30);
-    strokeWeight(2);
+    strokeWeight(4);
     noFill();
 
     // --- シャックル（U字の上部分）---
-    arc(0, -20, 100, 100, PI, 0);
+    arc(0, -20, 80, 80, PI, 0);
 
     // --- 本体 ---
     rectMode(CENTER);
-    rect(0, 40, 100, 120, 12);
+    rect(0, 30, 140, 100, 10);
 }
