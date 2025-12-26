8eee67db0afe53e873f54a65e026de4b20ee27e5
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2025-12-26 15:33:37 +0900

    Organize code into functions

--- js/sketch.js
+++ js/sketch.js
@@ -4,21 +4,40 @@ function setup() {
 
 function draw() {
     background('#f2f2f2');
+    translateToCenter();
+    setupDrawingStyle();
+    drawPadlock();
+}
 
+function translateToCenter() {
     translate(width / 2, height / 2); // 原点が画面中央になる。
-    
+}
+
+function setupDrawingStyle(){
     // --- 描画の初期設定 ---
     stroke(30);
     strokeWeight(4);
     noFill();
+}
 
+function drawPadlock() {
+    drawShackle();
+    drawPadlockBody();
+    drawKeyhole(); 
+}
+
+function drawShackle() {
     // --- シャックル（U字の上部分）---
     arc(0, -20, 80, 80, PI, 0);
+}
 
+function drawPadlockBody() {
     // --- 本体 ---
     rectMode(CENTER);
     rect(0, 30, 140, 100, 10);
+}
 
+function drawKeyhole() {
     // --- 鍵穴 ---
     fill(30);
     noStroke();
