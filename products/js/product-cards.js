0bc43fa27adc13201df30f21fc1d60d0fd62e3e3
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-03-11 13:05:55 +0900

    refactor: add class and anchor tags to product cards, update CSS, fix HTML formatting

--- products/js/product-cards.js
+++ products/js/product-cards.js
@@ -20,12 +20,18 @@ async function loadProducts() {
 
     /* Generate HTML */
     const html = products
-        .map(product => `
+        .map(product => {
+            // URLがある場合はリンクでラップ、無い場合はそのまま
+            const card = `
             <article class="product-card">
                 <img src="${product.icon ?? ""}" alt="${product.description ?? ""}" class="product-card__icon">
-                <h5>${product.name}</h5>
+                <h5 class="product-card__name">${product.name}</h5>
             </article>
-        `).join("");
+            `;
+            return product.url
+                ? `<a href="${product.url}" target="_blank" class="product-card__link">${card}</a>`
+                : card;
+        }).join("");
 
     /* Add to DOM */
     container.innerHTML = html;
