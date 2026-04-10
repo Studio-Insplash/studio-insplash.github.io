f0243495f4da293f0694ba67a494ed0ebf1b0a15
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-04-10 22:47:57 +0900

    feat: Add sub-filter innerHTML at genre condition and test functionality

--- products/js/filter.js
+++ products/js/filter.js
@@ -7,6 +7,8 @@ main.addEventListener("change", () => {
     }
     else if (main.value === "genre") {
         // process
+        const sub = document.getElementById("sub-filter")
+        sub.innerHTML = '<option value="">-- Select Sub --</option><option value="art">Art</option><option value="tool">Tool</option><option value="game">Game</option>'
     }
     else if (main.value === "language") {
         // process
