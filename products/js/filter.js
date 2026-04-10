0724b90116bde875ee06b4626ebe3a32c52596f8
Ju5tADeve10per <t0aruka1hat5u5ya@gmail.com>
2026-04-10 22:37:03 +0900

    feat: Implement if logic for main filter handling

--- products/js/filter.js
+++ products/js/filter.js
@@ -1,4 +1,17 @@
+console.log("filter loaded")
 const main = document.getElementById("main-filter")
+console.log(main)
 main.addEventListener("change", () => {
-    
+    if (main.value === "all") {
+        // process
+    }
+    else if (main.value === "genre") {
+        // process
+    }
+    else if (main.value === "language") {
+        // process
+    }
+    else if (main.value === "developer") {
+        // process
+    }
 })
